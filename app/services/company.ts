2778cf74d38cb65d332e95b94163161cb0d901a9
Charly Ghislain <code.editor>
2015-08-11 17:00:25 +0200

    correctly fetches company

--- app/services/company.ts
+++ app/services/company.ts
@@ -9,15 +9,12 @@ import {CompanyClient} from 'client/company';
 import {AuthService} from 'services/auth';
 
 export class CompanyService {
-    private data: Company[];
-
     private client:CompanyClient;
     private authService:AuthService;
 
     constructor(@Inject authService:AuthService) {
         this.client = new CompanyClient();
         this.authService = authService;
-        this.initFakeData();
     }
 
     createCompany(company: Company):Promise<CompanyRef> {
@@ -25,11 +22,7 @@ export class CompanyService {
         return this.client
             .createCompany(company, authToken)
             .then(function (companyRef:CompanyRef) {
-                console.log("Compagnie créée");
                 return companyRef;
-            }, function (error) {
-                console.log("Impossible de créer une compagnie");
-                console.log(error);
             }
         );
     }
@@ -37,36 +30,14 @@ export class CompanyService {
     getCompany(id: number): Promise<Company> {
         var thisService = this;
         var authToken = this.authService.authToken;
-        var promise = new Promise<Company>((resolve, reject) => {
-            var company = thisService.data[id];
-            resolve(company);
-        });
-        return promise;
+
+        return this.client.getCompany(id, authToken);
     }
 
     searchCompanies():Promise<Company[]> {
         // TODO
         var authToken = this.authService.authToken;
         var thisService = this;
-        var promise = new Promise<Company[]>((resolve, reject) => {
-            resolve(thisService.data);
-        });
-        return promise;
-    }
-
-    private initFakeData() {
-        var companyList = [];
-        var testCompany = new Company();
-        testCompany.id = 0;
-        testCompany.name ={'fr': 'Test'};
-        testCompany.description = {'fr': 'Compagnie de test'};
-        companyList[0] = testCompany;
-
-        var testCompany2 = new Company();
-        testCompany2.id=1;
-        testCompany2.name = {'fr': 'Test 2'}
-        testCompany2.description = {'fr': 'Deuxième compagnie de test'}
-        companyList[1] = testCompany2;
-        this.data = companyList;
+        return null;
     }
 }
