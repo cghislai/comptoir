c7ed546554e9ef55b55372cac6e35a795c900106
Charly Ghislain <code.editor>
2015-08-14 17:02:55 +0200

    fetch account for payview

--- app/components/sales/sale/payView/payView.ts
+++ app/components/sales/sale/payView/payView.ts
@@ -2,19 +2,19 @@
  * Created by cghislai on 29/07/15.
  */
 
-
 import {Component, View, EventEmitter, NgFor, NgIf} from 'angular2/angular2';
+
+import {Account, AccountRef,AccountSearch} from 'client/domain/account';
+import {SearchResult} from 'client/utils/search';
+import {AccountService} from 'services/account';
+import {ApplicationService} from 'services/application';
+
 import {Command} from 'services/saleService'
 import {AutoFocusDirective} from 'directives/autoFocus'
 
-enum PayMethod  {
-    CASH= 0,
-    CARD= 1,
-    PAYPAL= 2
-};
 class Pay {
-    method: PayMethod;
-    amount: number
+    account: Account;
+    amount: number;
 }
 
 @Component({
@@ -29,6 +29,9 @@ class Pay {
 })
 
 export class PayView {
+    accountService: AccountService;
+    language: string;
+
     command: Command;
     remainingAmount: number;
     paidAmount: number;
@@ -36,59 +39,63 @@ export class PayView {
     //cancelled= new EventEmitter();
 
     payList: Pay[];
-    availablePayMethods: PayMethod[];
+    allAccounts: Account[];
     editingPay: Pay;
 
-    constructor() {
+
+    constructor(accountService: AccountService, applicationService: ApplicationService) {
+        this.accountService = accountService;
+        this.language = applicationService.language.locale;
         this.reset();
     }
 
     reset() {
-        this.availablePayMethods = [];
-        this.availablePayMethods.push(PayMethod.CASH);
-        this.availablePayMethods.push(PayMethod.CARD);
-        this.availablePayMethods.push(PayMethod.PAYPAL);
         this.payList = [];
         this.editingPay = null;
         this.remainingAmount = undefined;
         this.paidAmount = 0;
+        this.searchAccounts();
     }
-    startEditPay(method: PayMethod) {
+
+    searchAccounts() {
+        var accountSearch = new AccountSearch();
+        // TODO: posRef
+        var thisView = this;
+        this.accountService.searchAccounts(accountSearch, null)
+        .then((result: SearchResult<Account>)=>{
+                thisView.allAccounts = result.list;
+            });
+    }
+
+    startEditPay(account: Account) {
         if (this.editingPay != null) {
             this.cancelEditPay();
         }
-        this.editingPay = this.removePayMethod(method);
-        if (this.editingPay == null) {
-            this.editingPay = new Pay();
-            this.editingPay.method = method;
-            this.editingPay.amount = this.remainingAmount;
-        }
-        this.setMethodAvailable(method, false);
+        this.editingPay = new Pay();
+        this.editingPay.account = account;
+        this.editingPay.amount = this.remainingAmount;
+
     }
     cancelEditPay() {
-        this.setMethodAvailable(this.editingPay.method, true);
         this.editingPay = null;
     }
     savePay() {
         if (this.editingPay.amount > 0) {
             this.payList.push(this.editingPay);
-        } else {
-            this.setMethodAvailable(this.editingPay.method, true);
         }
         this.editingPay = null;
         this.calcRemaining();
     }
     removePay(pay: Pay) {
         var newPayList : Pay[] = [];
-        this.payList.forEach(function(existingPay: Pay) {
+        for (var existingPay of this.payList) {
             if (existingPay == pay) {
                 return;
             }
             newPayList.push(existingPay);
-        })
+        }
         this.payList = newPayList;
         this.calcRemaining();
-        this.setMethodAvailable(pay.method, true);
     }
     calcRemaining() {
         var toPay = this.command.totalPrice;
@@ -108,32 +115,6 @@ export class PayView {
         paidAmount = Number((paidAmount).toFixed(2));
         return paidAmount;
     }
-    setMethodAvailable(method: PayMethod, available: boolean) {
-        var newMethods = [];
-        this.availablePayMethods.forEach(function(payMethod: PayMethod) {
-            if (payMethod == method) {
-                return;
-            }
-            newMethods.push(payMethod);
-        })
-        if (available) {
-            newMethods.push(method);
-        }
-        this.availablePayMethods = newMethods;
-    }
-    getPayMethodName(method: PayMethod) {
-        switch (method) {
-            case PayMethod.CARD: {
-                return "Card";
-            }
-            case PayMethod.CASH: {
-                return "Cash";
-            }
-            case PayMethod.PAYPAL: {
-                return "PayPal";
-            }
-        }
-    }
     onPayAmountKeyUp(amount: string, event) {
         var amountNumber : number = parseFloat(amount);
         switch (event.which) {
@@ -148,20 +129,7 @@ export class PayView {
                 }
         }
     }
-    removePayMethod(method: PayMethod) : Pay {
-        var newPays: Pay[] = [];
-        var removedPay:Pay = null
-        this.payList.forEach(function(pay: Pay) {
-            if (pay.method == method) {
-                removedPay = pay;
-                return;
-            }
-            newPays.push(pay);
-        })
-        this.payList = newPays;
-        this.calcRemaining();
-        return removedPay;
-    }
+
     onValidateClicked() {
         // TODO: validate the paiment
        this.reset();
