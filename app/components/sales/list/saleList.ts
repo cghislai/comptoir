cc4ba4d7439adc5761b7fe7d6f1abb460dd34dbd
Charly Ghislain <charlyghislain@gmail.com>
2015-09-28 11:21:19 +0200

    sale list

--- app/components/sales/list/saleList.ts
+++ app/components/sales/list/saleList.ts
@@ -2,16 +2,18 @@
  * Created by cghislai on 29/07/15.
  */
 
-import {Component, View, NgFor, NgIf,
-    EventEmitter, Attribute, ViewEncapsulation} from 'angular2/angular2';
+import {Component, View, NgFor, NgIf, NgSwitch, NgSwitchWhen,
+    ChangeDetectionStrategy,
+    EventEmitter, ViewEncapsulation} from 'angular2/angular2';
 
 import {LocalSale} from 'client/localDomain/sale';
-import {LocaleTexts} from 'client/utils/lang';
+import {Language, LanguageFactory, LocaleTexts, LocaleTextsFactory} from 'client/utils/lang';
 
 import {AuthService} from 'services/auth';
 
-import {AutoFocusDirective} from 'components/utils/autoFocus';
 import {FocusableDirective} from 'components/utils/focusable';
+
+import {List} from 'immutable';
 
 
 /****
@@ -20,21 +22,26 @@ import {FocusableDirective} from 'components/utils/focusable';
 @Component({
     selector: "saleColumn",
     properties: ['sale', 'column', 'lang'],
-    events: ['action']
+    events: ['action'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 @View({
     templateUrl: './components/sales/list/saleColumn.html',
     styleUrls: ['./components/sales/list/saleList.css'],
-    directives: [NgIf],
+    directives: [NgIf, NgSwitch, NgSwitchWhen],
     // eases styling
     encapsulation: ViewEncapsulation.None
 })
 export class SaleColumnComponent {
     action = new EventEmitter();
+    sale:LocalSale;
+    column:SaleColumn;
+    lang:Language;
 
     onColumnAction(sale:LocalSale, column:SaleColumn, event) {
         this.action.next({sale: sale, column: column});
-        //  event.stopPropagation();
+        event.stopPropagation();
+        event.preventDefault();
     }
 
 }
@@ -46,44 +53,36 @@ export class SaleColumnComponent {
 
 @Component({
     selector: 'saleList',
-    properties: ['sales', 'columns', 'selectableParam: selectable', 'headersParams: headers'],
-    events: ['saleClicked', 'columnAction']
+    properties: ['sales', 'columns', 'rowSelectable', 'headersVisible'],
+    events: ['saleClicked', 'columnAction'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 
 @View({
     templateUrl: './components/sales/list/saleList.html',
     styleUrls: ['./components/sales/list/saleList.css'],
-    directives: [NgFor, NgIf, AutoFocusDirective, FocusableDirective, SaleColumnComponent]
+    directives: [NgFor, NgIf, FocusableDirective, SaleColumnComponent]
 })
 
 export class SaleListComponent {
     // properties
-    sales:LocalSale[];
-    columns:SaleColumn[];
+    sales:List<LocalSale>;
+    columns:List<SaleColumn>;
     selectable:boolean;
     headers:boolean;
+    language:Language;
 
     saleClicked = new EventEmitter();
     columnAction = new EventEmitter();
-    locale:string;
 
     constructor(authService:AuthService) {
-        this.locale = authService.getEmployeeLanguage().locale;
+        this.language = authService.getEmployeeLanguage();
     }
 
-    set headersParams(value: string) {
-        this.headers = value != 'false';
-    }
-
-    set selectableParam(value: string) {
-        this.selectable = value != 'false';
-    }
-
-    onSaleClick(sale: LocalSale, col: SaleColumn) {
-        if (col == SaleColumn.ACTION_REMOVE) {
-            return;
-        }
+    onSaleClick(sale:LocalSale, event) {
         this.saleClicked.next(sale);
+        event.stopPropagation();
+        event.preventDefault();
     }
 
     onColumnAction(event:any) {
@@ -94,15 +93,15 @@ export class SaleListComponent {
 
 export class SaleColumn {
 
-    static ID: SaleColumn;
-    static DATETIME: SaleColumn;
-    static VAT_EXCLUSIVE_AMOUNT: SaleColumn;
-    static VAT_INCLUSIVE_AMOUNT: SaleColumn;
-    static VAT_AMOUNT: SaleColumn;
-    static CLOSED: SaleColumn;
+    static ID:SaleColumn;
+    static DATETIME:SaleColumn;
+    static VAT_EXCLUSIVE_AMOUNT:SaleColumn;
+    static VAT_INCLUSIVE_AMOUNT:SaleColumn;
+    static VAT_AMOUNT:SaleColumn;
+    static CLOSED:SaleColumn;
     static REFERENCE:SaleColumn;
-    static ACTION_REMOVE: SaleColumn;
-    
+    static ACTION_REMOVE:SaleColumn;
+
     title:LocaleTexts;
     name:string;
     alignRight:boolean;
@@ -110,50 +109,48 @@ export class SaleColumn {
     static init() {
         SaleColumn.ID = new SaleColumn();
         SaleColumn.ID.name = 'id';
-        SaleColumn.ID.title = {
+        SaleColumn.ID.title = LocaleTextsFactory.toLocaleTexts({
             'fr': "Id"
-        };
+        });
 
         SaleColumn.DATETIME = new SaleColumn();
         SaleColumn.DATETIME.name = 'dateTime';
-        SaleColumn.DATETIME.title = {
+        SaleColumn.DATETIME.title = LocaleTextsFactory.toLocaleTexts({
             'fr': "Date"
-        };
+        });
 
         SaleColumn.VAT_EXCLUSIVE_AMOUNT = new SaleColumn();
         SaleColumn.VAT_EXCLUSIVE_AMOUNT.name = 'vatExclusive';
         SaleColumn.VAT_EXCLUSIVE_AMOUNT.alignRight = true;
-        SaleColumn.VAT_EXCLUSIVE_AMOUNT.title = {
+        SaleColumn.VAT_EXCLUSIVE_AMOUNT.title = LocaleTextsFactory.toLocaleTexts({
             'fr': "Total HTVA"
-        };
+        });
 
         SaleColumn.VAT_INCLUSIVE_AMOUNT = new SaleColumn();
         SaleColumn.VAT_INCLUSIVE_AMOUNT.name = 'vatInclusive';
         SaleColumn.VAT_INCLUSIVE_AMOUNT.alignRight = true;
-        SaleColumn.VAT_INCLUSIVE_AMOUNT.title = {
+        SaleColumn.VAT_INCLUSIVE_AMOUNT.title = LocaleTextsFactory.toLocaleTexts({
             'fr': "Total"
-        };
+        });
 
         SaleColumn.VAT_AMOUNT = new SaleColumn();
         SaleColumn.VAT_AMOUNT.name = 'vat';
         SaleColumn.VAT_AMOUNT.alignRight = true;
-        SaleColumn.VAT_AMOUNT.title = {
-        'fr': "TVA"
-        };
+        SaleColumn.VAT_AMOUNT.title = LocaleTextsFactory.toLocaleTexts({
+            'fr': "TVA"
+        });
 
         SaleColumn.CLOSED = new SaleColumn();
         SaleColumn.CLOSED.name = 'closed';
-        SaleColumn.CLOSED.title = {
+        SaleColumn.CLOSED.title = LocaleTextsFactory.toLocaleTexts({
             'fr': "Clôturée"
-        };
+        });
 
         SaleColumn.REFERENCE = new SaleColumn();
         SaleColumn.REFERENCE.name = 'ref';
-        SaleColumn.REFERENCE.title = {
+        SaleColumn.REFERENCE.title = LocaleTextsFactory.toLocaleTexts({
             'fr': "Référence"
-        };
-
-
+        });
 
         SaleColumn.ACTION_REMOVE = new SaleColumn();
         SaleColumn.ACTION_REMOVE.name = 'action_remove';
